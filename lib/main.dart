94a5fe849d16611bec51164c28f80f23c228a0f7
Alec Dello Iacono <157432683+alecdel@users.noreply.github.com>
2024-09-15 19:48:40 -0400

    Initial commit of the Flutter calculator app

---
+++ lib/main.dart
@@ -0,0 +1,146 @@
+import 'package:flutter/material.dart';
+import 'package:expressions/expressions.dart';
+
+void main() {
+  runApp(SimpleCalculatorApp());
+}
+
+class SimpleCalculatorApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Alec Dello Iacono - Calculator',
+      theme: ThemeData(
+        // color of digits
+        primarySwatch: Colors.blue,
+      ),
+      home: CalculatorScreen(),
+    );
+  }
+}
+
+class CalculatorScreen extends StatefulWidget {
+  @override
+  _CalculatorScreenState createState() => _CalculatorScreenState();
+}
+
+class _CalculatorScreenState extends State<CalculatorScreen> {
+  String _expression = '';
+  String _result = '';
+
+  void _onButtonPressed(String value) {
+    setState(() {
+      _expression += value;
+    });
+  }
+
+  void _clear() {
+    setState(() {
+      _expression = '';
+      _result = '';
+    });
+  }
+
+  void _calculate() {
+    try {
+      final expression = Expression.parse(_expression);
+      final evaluator = const ExpressionEvaluator();
+      final evalResult = evaluator.eval(expression, {});
+
+      setState(() {
+        _result = evalResult.toString();
+      });
+    } catch (e) {
+      setState(() {
+        _result = 'Error';
+      });
+    }
+  }
+
+  Widget _buildButton(String text, {Color color = Colors.blue}) {
+    return ElevatedButton(
+      onPressed: text == '='
+          ? _calculate
+          : text == 'C'
+              ? _clear
+              : () => _onButtonPressed(text),
+      style: ElevatedButton.styleFrom(
+        padding: EdgeInsets.all(20),
+        textStyle: TextStyle(fontSize: 24),
+        backgroundColor: color,
+      ),
+      child: Text(text),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Alec Dello Iacono - Calculator'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(16),
+              alignment: Alignment.centerRight,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Text(
+                    _expression,
+                    style: TextStyle(fontSize: 32, color: Colors.black54),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    _result,
+                    style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              _buildButton('7'),
+              _buildButton('8'),
+              _buildButton('9'),
+              _buildButton('/', color: Colors.orange),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              _buildButton('4'),
+              _buildButton('5'),
+              _buildButton('6'),
+              _buildButton('*', color: Colors.orange),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              _buildButton('1'),
+              _buildButton('2'),
+              _buildButton('3'),
+              _buildButton('-', color: Colors.orange),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              _buildButton('0'),
+              _buildButton('C', color: Colors.red),
+              _buildButton('=', color: Colors.green),
+              _buildButton('+', color: Colors.orange),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
